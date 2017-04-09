303c9eea40401a36eaaf8ee56acce539b225d8b2
ninjaotoko <xavierlesa@gmail.com>
2017-04-09 16:34:15 -0300

    [test] json for ui

--- iotdevice/views.py
+++ iotdevice/views.py
@@ -50,6 +50,12 @@ class PIDAutoView(JSONResponseMixin, DetailView):
         return super(PIDAutoView, self).dispatch(*args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        params = request.POST
+        print("publish to 260c4ad9-a5ae-49e6-95ec-b9bc643d1049/onoff => %s" % params)
+        #device_publish_signal.send(sender=self, 
+        #        device_topic="260c4ad9-a5ae-49e6-95ec-b9bc643d1049/onoff", 
+        #        message=str(onoff)
+        #    )
         return self.render_to_response({})
 
 
